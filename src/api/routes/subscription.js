0df69143cf7be56a4bf65629610947bebcbc01ae
haRsh-cse-rgb <rockingharsh305@gmail.com>
2025-09-01 17:03:53 +0530

    Added

--- src/api/routes/subscription.js
+++ src/api/routes/subscription.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const subscriptionController = require('../controllers/subscriptionController');
 
 // POST /api/v1/subscribe - Subscribe to newsletter
-router.post('/', subscriptionController.subscribe);
+router.post('/subscribe', subscriptionController.subscribe);
 
 module.exports = router;
